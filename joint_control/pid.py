00c74df1201c0f47df45da27f424ff7bda0e4db6
Emre Yurtseven <emreyurtseven18@gmail.com>
2022-11-08 22:29:54 +0100

    push pid implementation

--- joint_control/pid.py
+++ joint_control/pid.py
@@ -1,4 +1,4 @@
-'''In this exercise you need to implement the PID controller for joints of robot.
+"""In this exercise you need to implement the PID controller for joints of robot.
 
 * Task:
     1. complete the control function in PIDController with prediction
@@ -7,12 +7,17 @@
 * Hints:
     1. the motor in simulation can simple modelled by angle(t) = angle(t-1) + speed * dt
     2. use self.y to buffer model prediction
-'''
+"""
 
 # add PYTHONPATH
 import os
 import sys
-sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'software_installation'))
+
+sys.path.append(
+    os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), "..", "software_installation"
+    )
+)
 
 import numpy as np
 from collections import deque
@@ -20,15 +25,16 @@ from spark_agent import SparkAgent, JOINT_CMD_NAMES
 
 
 class PIDController(object):
-    '''a discretized PID controller, it controls an array of servos,
-       e.g. input is an array and output is also an array
-    '''
+    """a discretized PID controller, it controls an array of servos,
+    e.g. input is an array and output is also an array
+    """
+
     def __init__(self, dt, size):
-        '''
+        """
         @param dt: step time
         @param size: number of control values
         @param delay: delay in number of steps
-        '''
+        """
         self.dt = dt
         self.u = np.zeros(size)
         self.e1 = np.zeros(size)
@@ -41,29 +47,43 @@ class PIDController(object):
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
     def set_delay(self, delay):
-        '''
+        """
         @param delay: delay in number of steps
-        '''
+        """
         self.y = deque(self.y, delay + 1)
 
     def control(self, target, sensor):
-        '''apply PID control
+        """apply PID control
         @param target: reference values
         @param sensor: current values from sensor
         @return control signal
-        '''
+        """
         # YOUR CODE HERE
+        dsensor = sensor + self.u * self.dt
+        self.y.appendleft(dsensor)
+        e = target - dsensor
 
+        # Formel von dem Vorlesungsvideo
+        self.u += (self.Kp + self.Ki * self.dt + self.Kd / self.dt) * e
+        self.u -= (self.Kp + 2 * self.Kd / self.dt) * self.e1
+        self.u += self.Kd / self.dt * self.e2
+        self.e2 = self.e1
+        self.e1 = e
         return self.u
 
 
 class PIDAgent(SparkAgent):
-    def __init__(self, simspark_ip='localhost',
-                 simspark_port=3100,
-                 teamname='DAInamite',
-                 player_id=0,
-                 sync_mode=True):
-        super(PIDAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
+    def __init__(
+        self,
+        simspark_ip="localhost",
+        simspark_port=3100,
+        teamname="DAInamite",
+        player_id=0,
+        sync_mode=True,
+    ):
+        super(PIDAgent, self).__init__(
+            simspark_ip, simspark_port, teamname, player_id, sync_mode
+        )
         self.joint_names = JOINT_CMD_NAMES.keys()
         number_of_joints = len(self.joint_names)
         self.joint_controller = PIDController(dt=0.01, size=number_of_joints)
@@ -71,19 +91,24 @@ class PIDAgent(SparkAgent):
 
     def think(self, perception):
         action = super(PIDAgent, self).think(perception)
-        '''calculate control vector (speeds) from
+        """calculate control vector (speeds) from
         perception.joint:   current joints' positions (dict: joint_id -> position (current))
-        self.target_joints: target positions (dict: joint_id -> position (target)) '''
+        self.target_joints: target positions (dict: joint_id -> position (target)) """
         joint_angles = np.asarray(
-            [perception.joint[joint_id]  for joint_id in JOINT_CMD_NAMES])
-        target_angles = np.asarray([self.target_joints.get(joint_id, 
-            perception.joint[joint_id]) for joint_id in JOINT_CMD_NAMES])
+            [perception.joint[joint_id] for joint_id in JOINT_CMD_NAMES]
+        )
+        target_angles = np.asarray(
+            [
+                self.target_joints.get(joint_id, perception.joint[joint_id])
+                for joint_id in JOINT_CMD_NAMES
+            ]
+        )
         u = self.joint_controller.control(target_angles, joint_angles)
         action.speed = dict(zip(JOINT_CMD_NAMES.keys(), u))  # dict: joint_id -> speed
         return action
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     agent = PIDAgent()
-    agent.target_joints['HeadYaw'] = 1.0
+    agent.target_joints["HeadYaw"] = 1.0
     agent.run()

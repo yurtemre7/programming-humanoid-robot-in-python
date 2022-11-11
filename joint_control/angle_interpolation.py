e868c02e9915916536fa018a9b058817c568923d
Emre Yurtseven <emreyurtseven18@gmail.com>
2022-11-11 19:56:23 +0100

    implement angle interpolation

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -1,4 +1,4 @@
-'''In this exercise you need to implement an angle interploation function which makes NAO executes keyframe motion
+"""In this exercise you need to implement an angle interploation function which makes NAO executes keyframe motion
 
 * Tasks:
     1. complete the code in `AngleInterpolationAgent.angle_interpolation`,
@@ -17,35 +17,86 @@
     # where Handle is [int InterpolationType, float dTime, float dAngle] describing the handle offsets relative
     # to the angle and time of the point. The first Bezier param describes the handle that controls the curve
     # preceding the point, the second describes the curve following the point.
-'''
+"""
 
 
 from pid import PIDAgent
-from keyframes import hello
+from keyframes import *
+
+# formula of bezier interpolation
+def bezier(p_0, p_1, p_2, p_3, t):
+    return (
+        (1 - t) ** 3 * p_0
+        + 3 * (1 - t) ** 2 * t * p_1
+        + 3 * (1 - t) * t**2 * p_2
+        + t**3 * p_3
+    )
 
 
 class AngleInterpolationAgent(PIDAgent):
-    def __init__(self, simspark_ip='localhost',
-                 simspark_port=3100,
-                 teamname='DAInamite',
-                 player_id=0,
-                 sync_mode=True):
-        super(AngleInterpolationAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
+    def __init__(
+        self,
+        simspark_ip="localhost",
+        simspark_port=3100,
+        teamname="DAInamite",
+        player_id=0,
+        sync_mode=True,
+    ):
+        super(AngleInterpolationAgent, self).__init__(
+            simspark_ip, simspark_port, teamname, player_id, sync_mode
+        )
         self.keyframes = ([], [], [])
+        self.begin = None
 
     def think(self, perception):
         target_joints = self.angle_interpolation(self.keyframes, perception)
-        target_joints['RHipYawPitch'] = target_joints['LHipYawPitch'] # copy missing joint in keyframes
+        target_joints["RHipYawPitch"] = target_joints.get(
+            "LHipYawPitch", 0
+        )  # copy missing joint in keyframes
         self.target_joints.update(target_joints)
         return super(AngleInterpolationAgent, self).think(perception)
 
     def angle_interpolation(self, keyframes, perception):
         target_joints = {}
+
         # YOUR CODE HERE
+        names, times, keys = keyframes
+
+        if self.begin is None:
+            self.begin = perception.time
+
+        dt = perception.time - self.begin
+
+        for i in range(len(names)):
+            joint_name = names[i]
+
+            if joint_name in self.joint_names:
+                time = times[i]
+                key = keys[i]
+
+                timepoints = len(time)
+
+                # go through all timepoints
+                for tp in range(timepoints - 1):
+                    if time[tp] < dt < time[tp + 1]:
+                        p_0 = key[tp][0]
+                        p_1 = p_0 + (key[tp][2][1] * key[tp][2][2])
+                        p_3 = key[tp + 1][0]
+                        p_2 = p_3 + (key[tp + 1][1][1] * key[tp + 1][1][2])
+                        tdt = (dt - time[tp]) / (time[tp + 1] - time[tp])
+                        target_joints[joint_name] = bezier(p_0, p_1, p_2, p_3, tdt)
+                    # for the first keyframe
+                    elif dt < time[0]:
+                        p_0 = perception.joint[joint_name]
+                        p_3 = key[0][0]
+                        p_2 = p_3 + (key[tp][2][1] * key[tp][2][2])
+                        tdt = dt / time[1]
+                        target_joints[joint_name] = bezier(p_0, p_0, p_2, p_3, tdt)
 
         return target_joints
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     agent = AngleInterpolationAgent()
     agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
     agent.run()
